6a0e4957249110adf491c09fb66fd031d7440056
Soukaina Abouelmir <abouelmirsoukaina2@gmail.com>
2024-04-25 12:15:49 +0100

    responsive navsidebar

--- resources/views/etudiant/layouts/navbaretudiant.blade.php
+++ resources/views/etudiant/layouts/navbaretudiant.blade.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Etudiant</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
@@ -155,24 +150,7 @@ height:auto;
 
 <body>
     <!--sidebar horizontal
-    <nav class="navbar">
-        <div class="container">
-            <img class="m-0 p-0" src="{{ asset('asset/images/logo.webp') }}" alt="suptech logo" width="15%">
-            <div class="navbar-left">
-                <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor"
-                    class="bi bi-flag-fill" viewBox="0 0 16 16" style="color: #173165;">
-                    <path
-                        d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12 12 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A20 20 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a20 20 0 0 0 1.349-.476l.019-.007.004-.002h.001" />
-                </svg>
-                <a class="navbar-item p-5" href="#" style="text-decoration: none;">Nom utilisateur
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
-                    class="bi bi-person-fill icon-style" viewBox="0 0 16 16" style="color: #173165;">
-                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                </svg></a>
-            </div>
-        </div>
-    </nav>
-< HEAD>
+   
     
     
 =======
@@ -316,6 +294,4 @@ height:auto;
     </script>
     
    
-</body>
 
-</html>
