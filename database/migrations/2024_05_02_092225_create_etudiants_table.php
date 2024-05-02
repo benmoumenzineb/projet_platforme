f59c486a4e08fbf65efe4c1e1760724f21c6d059
iamfaty <fatimaezzahraelbouazizi@gmail.com>
2024-05-02 11:25:52 +0100

    profil etudiant bd

--- database/migrations/2024_05_02_092225_create_etudiants_table.php
+++ database/migrations/2024_05_02_092225_create_etudiants_table.php
@@ -15,6 +15,37 @@ return new class extends Migration
     {
         Schema::create('etudiants', function (Blueprint $table) {
             $table->id();
+             $table->string('Etablissement');
+             /*
+             etudiant
+             */
+            $table->Text('Code_Apogee');
+            $table->text('Cne');
+            $table->Text('Cycle');
+            $table->date('Date_inscription');
+            $table->Text('Nom');
+            $table->Text('Prenom');
+            $table->date('Date_naissance');
+            $table->text('Sexe');
+            $table->date('Lieu_naissance');
+            $table->text('Cni');
+            $table->text('Adresse');
+            $table->text('Email');
+            $table->text('Telephone');
+            /*
+             parent
+             */
+            $table->text('Nom_pere');
+            $table->text('Prenom_pere');
+            $table->text('Profesion_pere');
+            $table->text('Telephone_pere');
+            $table->text('Nom_mere');
+            $table->text('Prenom_mere');
+            $table->text('Profesion_mere');
+            $table->text('Telephone_mere');
+            $table->text('Nom_tuteur');
+            $table->text('Telephone_tuteur');
+          
             $table->timestamps();
         });
     }
