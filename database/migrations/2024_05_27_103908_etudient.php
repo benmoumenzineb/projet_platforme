88137864aed9f4146526959bacbe2f1d1ebb7384
iamfaty <fatimaezzahraelbouazizi@gmail.com>
2024-05-27 16:41:54 +0100

    test

---
+++ database/migrations/2024_05_27_103908_etudient.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $inscriptions = DB::table('inscriptions')->select('id', 'apogee')->get();
+
+        // Mettre à jour les étudiants avec les codes apogée correspondants
+        foreach ($inscriptions as $inscription) {
+            DB::table('etudient')->where('id', $inscription->id)->update([
+                'apogee' => $inscription->apogee,
+            ]);
+    }}
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
